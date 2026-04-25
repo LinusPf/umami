ec99e2df2f117f9f5747b573dcd6c947a8c027a5
LinusPf <35813817+LinusPf@users.noreply.github.com>
2026-04-25 20:04:26 +0200

    Aktualisieren von prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
   datasource: {
-    url: env('postgresql://postgres.malutiofpxorlrviiecw:n7NGPb&p60pW8O9.HgYh@aws-0-eu-west-1.pooler.supabase.com:6543/postgres'),
-    directUrl: env('postgresql://postgres:n7NGPb&p60pW8O9.HgYh@db.malutiofpxorlrviiecw.supabase.co:5432/postgres'),
+    url: env('DATABASE_URL'),
+    directUrl: env('DIRECT_DATABASE_URL'),
   },
 });
